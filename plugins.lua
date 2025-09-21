327c50d48cc74b09389b55a37608cc999b892817
realtime74 <realtime74@proton.me>
2025-09-21 08:09:17 +0200

    use pckr

--- plugins.lua
+++ plugins.lua
@@ -1,11 +1,22 @@
-return {
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  {
-    "github/copilot.vim",
-  },
-  {
-    "tpope/vim-fugitive",
-  },
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
+  end
+
+  vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
+
+require('pckr').add{
+  { "github/copilot.vim", },
+  { "tpope/vim-fugitive", },
 }
